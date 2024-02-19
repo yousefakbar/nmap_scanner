bf7b918c6c47cb518672a444242969b9839f4dcb
yousefakbar <yhakbar99@gmail.com>
2024-02-19 10:32:50 +0300

    Style fixes in main.py

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import sys
 from PyQt5.QtWidgets import QApplication
 from nmapscanner import NmapScanner
 
+
 def main():
     app = QApplication(sys.argv)
     ex = NmapScanner()
     ex.show()
     sys.exit(app.exec_())
+
 
 if __name__ == '__main__':
     main()
