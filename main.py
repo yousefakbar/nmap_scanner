822166c96d7b0cae276d73079e00c992bb2f25ed
Abdulaziz Alabdulrazzaq <alabdulrazzaq.abdulaziz@gmail.com>
2024-03-03 07:40:47 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from nmapscanner import NmapScanner
 
 def main():
     app = QApplication(sys.argv)
+    sshFile = "NMapScannerCSS.qss"
+    with open(sshFile, "r") as fh:
+        app.setStyleSheet(fh.read())
     ex = NmapScanner()
     ex.show()
     sys.exit(app.exec_())
