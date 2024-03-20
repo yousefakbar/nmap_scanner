751098f50e886fa563480469cb7c4d245e024a04
Khalid <kfg7.2000@gmail.com>
2024-03-20 12:02:48 +0300

    Add User class

---
+++ models/user.py
@@ -0,0 +1,6 @@
+# TODO: Autheticate user and track user scans
+class User():
+    def __init__(self, username, name, previous_scans):
+        self.username = username
+        self.name = name
+        self.previous_scans = previous_scans
