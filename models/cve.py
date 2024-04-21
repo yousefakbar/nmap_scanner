8f60d15b04e9b8175d9b113d8229f7f436976c64
Abdulaziz Alabdulrazzaq <alabdulrazzaq.abdulaziz@gmail.com>
2024-04-21 07:26:01 +0000

    Replace cve.py

--- models/cve.py
+++ models/cve.py
@@ -1,7 +1,6 @@
 class CVE():
     def __init__(self, name, last_modified, url, severity):
-    self.name = name
-    self.last_modified = last_modified
-    self.url = url
-    self.severity = severity
-    
+        self.name = name
+        self.last_modified = last_modified
+        self.url = url
+        self.severity = severity
